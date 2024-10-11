02a09d32fb0f504d34602b82bfc08d516ca48bcb
mQzLjP <91550006+mQzLjP@users.noreply.github.com>
2024-10-11 08:36:08 +0800

    fix: completely remove content on empty stage (#45)
    Co-authored-by: BoYanZh <boyanzh233@gmail.com>

--- joint_teapot/utils/joj3.py
+++ joint_teapot/utils/joj3.py
@@ -196,12 +196,12 @@ def generate_title_and_comment(
         + "[Joint-Teapot](https://github.com/BoYanZh/Joint-Teapot) with ❤️.\n"
     )
     for stage in stages:
-        comment += f"## {stage['name']}\n"
         if all(
             result["score"] == 0 and result["comment"].strip() == ""
             for result in stage["results"]
         ):
             continue
+        comment += f"## {stage['name']}\n"
         force_quit = stage["force_quit"]
         if force_quit:
             comment += " - Failed"
