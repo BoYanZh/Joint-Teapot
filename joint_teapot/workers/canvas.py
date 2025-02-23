c0dc28d19cf8934e2bfd4f907c14248c6b41bff3
Min Zhengjie <minzhengjie@sjtu.edu.cn>
2025-02-23 15:40:00 +0800

    feat: keep the ASCII names

--- joint_teapot/workers/canvas.py
+++ joint_teapot/workers/canvas.py
@@ -1,4 +1,5 @@
 import os
+import re
 from glob import glob
 from typing import cast
 
@@ -33,6 +34,9 @@ class Canvas:
         types = ["student"]
 
         def patch_student(student: User) -> User:
+            student.name = re.sub(
+                r"[^\x00-\x7F]+", "", student.name
+            ).strip()  # We only care english name
             student.sis_id = student.login_id
             student.login_id = student.email.split("@")[0]
             return student
